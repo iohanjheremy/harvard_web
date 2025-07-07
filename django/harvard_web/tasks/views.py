b70a147556129f2115ddfdcebfa060825c02f086
iohanjheremy <iohanjheremy11@gmail.com>
2025-07-07 13:31:34 -0400

    Novas funções e heranças de templates

--- django/harvard_web/tasks/views.py
+++ django/harvard_web/tasks/views.py
@@ -1,8 +1,18 @@
+from django import forms
 from django.shortcuts import render
 
-tasks = ['foo', 'bar', 'baz']
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
+
+tasks = ['Learn Django', 'GYM', 'Play Guitar', 'Study Python', 'Watch a movie']
 # Create your views here.
 def index (request):
-    render(request, "tasks/index.html", {
+    return render(request, "tasks/index.html", {
         "tasks": tasks 
+    })
+
+def add (request):
+    return render(request, "tasks/add.html",{
+        "form": NewTaskForm()
     })
