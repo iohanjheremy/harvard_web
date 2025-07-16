863dfe2c85e4f59073322c77a1be66e843045cfc
iohanjheremy <iohanjheremy11@gmail.com>
2025-07-16 13:36:18 -0400

    Criando autenticação e estilizando a página flights

--- django/airline/airline/users/views.py
+++ django/airline/airline/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -10,7 +11,17 @@ def login_view(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-    return render(request, "user/login.html")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html",{
+                "message": "Invalid credentials."
+            })
+            
+    return render(request, "users/login.html")
 
 def logout_view(request):
     pass
