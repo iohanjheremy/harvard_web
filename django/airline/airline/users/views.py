b6427b2d6ac30d1df20aeb29be8b2af45aff8112
iohanjheremy <iohanjheremy11@gmail.com>
2025-07-17 13:31:20 -0400

    Validação de login

--- django/airline/airline/users/views.py
+++ django/airline/airline/users/views.py
@@ -15,7 +15,7 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse("index"))
+            return HttpResponseRedirect(reverse("login"))
         else:
             return render(request, "users/login.html",{
                 "message": "Invalid credentials."
