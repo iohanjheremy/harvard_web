2fb87ed84b0939aa255a75c6b3f005ea709b6881
iohanjheremy <iohanjheremy11@gmail.com>
2025-07-22 11:42:36 -0400

    FInalizando login e logout

--- django/airline/users/urls.py
+++ django/airline/users/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout")
+    path("login/", views.login_view, name="user_login"),
+    path("logout/", views.logout_view, name="user_logout")
 ]
--- django/airline/users/views.py
+++ django/airline/users/views.py
@@ -1,17 +1,27 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 # Create your views here.
 def index (request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
+    return render(request, "users/user.html")
     
 def login_view(request):
     if request.method == "POST":
-        username = request.POST["username"]
+        username = request.POST["username"].strip()
         password = request.POST["password"]
+        print("Usuário:", request.POST["username"])
+        print("Senha:", request.POST["password"])
+
         user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            print("Login bem-sucedido")
+        else:
+            print("Falha na autenticação")
+
 
         if user is not None:
             login(request, user)
@@ -24,4 +34,7 @@ def login_view(request):
     return render(request, "users/login.html")
 
 def logout_view(request):
-    pass
+    if request.method == "POST":
+        logout(request)
+        return redirect("user_login")
+    return redirect("index")
